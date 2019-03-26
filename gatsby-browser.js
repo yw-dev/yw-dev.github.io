73b1199ea046624990ccdc74c70a0195735b9ebe
yw-dev <1775216@gmail.com>
2019-03-26 22:11:45 +0800

    #offline up

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,14 @@
 
 // You can delete this file if you're not using it
 require("prismjs/themes/prism-solarizedlight.css")
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `网站有新数据更新. ` +
+        `是否加载最新的数据?`
+    )
+  
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
